a7a31163303330cf832cab9f214894f37b73b0e7
Lucas Bandeira <lucas4workl8tr@gmail.com>
2022-08-28 19:43:40 -0300

    criacao de tabela de simbolos

--- atividadeComplementar.py
+++ atividadeComplementar.py
@@ -1,3 +1,5 @@
+from tabulate import tabulate
+
 tokens = []
 
 special_characters = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=',
@@ -7,13 +9,15 @@ string = input("Digite um lexeme (uma tecla qualquer ou # para sair): ")
 string_replaced = string.replace(" ", "")
 string_size = len(string_replaced)
 
+table = [['ID', ' ']]
+
 i = 0
 count = 1
 countOccurrences = 0
 
 while i < string_size:
     lexeme = string_replaced[i]
-    # if lexeme == "+" or lexeme == "=" or lexeme == "*" or lexeme == "-":
+
     if lexeme in special_characters:
         if countOccurrences == 0:
             if string_replaced[i - 1].isdigit():
@@ -22,6 +26,10 @@ while i < string_size:
                 i += 1
                 countOccurrences = i
             else:
+                if string_replaced[0:i] in table:
+                    print('esta na lista')
+                # table.append('{0}   {1}'.format(count, string_replaced[0:i]))
+                table.append(['{0}'.format(count), '{0}'.format(string_replaced[0:i])])
                 tokens.append("<id, %s>" % count)
                 tokens.append("<%s>" % lexeme)
                 i += 1
@@ -34,6 +42,10 @@ while i < string_size:
                 i += 1
                 countOccurrences = i
             else:
+                if string_replaced[countOccurrences:i] in table:
+                    print('esta na lista')
+                # table.append('{0}   {1}'.format(count, string_replaced[countOccurrences:i]))
+                table.append(['{0}'.format(count), '{0}'.format(string_replaced[countOccurrences:i])])
                 tokens.append("<id, %s>" % count)
                 tokens.append("<%s>" % lexeme)
                 i += 1
@@ -46,6 +58,10 @@ while i < string_size:
             i += 1
             countOccurrences = i
         else:
+            if string_replaced[countOccurrences:i + 1] in table:
+                print('esta na lista')
+            # table.append('{0}   {1}'.format(count, string_replaced[countOccurrences:i + 1]))
+            table.append(['{0}'.format(count), '{0}'.format(string_replaced[countOccurrences:i + 1])])
             tokens.append("<id, %s>" % count)
             i += 1
             count += 1
@@ -54,4 +70,6 @@ while i < string_size:
     else:
         i += 1
 
+print("table:", tabulate(table))
+# print("list:", table)
 print("tokens:", tokens)
