33c55181076daf37bb0b580cc2e0f3fa94ea1efe
Lucas Bandeira <lucas4workl8tr@gmail.com>
2022-08-30 18:02:57 -0300

    filtro de simbolos iguais

--- atividadeComplementar.py
+++ atividadeComplementar.py
@@ -26,9 +26,6 @@ while i < string_size:
                 i += 1
                 countOccurrences = i
             else:
-                if string_replaced[0:i] in table:
-                    print('esta na lista')
-                # table.append('{0}   {1}'.format(count, string_replaced[0:i]))
                 table.append(['{0}'.format(count), '{0}'.format(string_replaced[0:i])])
                 tokens.append("<id, %s>" % count)
                 tokens.append("<%s>" % lexeme)
@@ -36,36 +33,59 @@ while i < string_size:
                 count += 1
                 countOccurrences = i
         else:
-            if string_replaced[i - 1].isdigit():
-                tokens.append("<%s>" % string_replaced[countOccurrences:i])
-                tokens.append("<%s>" % lexeme)
-                i += 1
-                countOccurrences = i
-            else:
-                if string_replaced[countOccurrences:i] in table:
-                    print('esta na lista')
-                # table.append('{0}   {1}'.format(count, string_replaced[countOccurrences:i]))
-                table.append(['{0}'.format(count), '{0}'.format(string_replaced[countOccurrences:i])])
-                tokens.append("<id, %s>" % count)
-                tokens.append("<%s>" % lexeme)
-                i += 1
-                count += 1
-                countOccurrences = i
+            for array in table:
+                if string_replaced[countOccurrences:i] in array:
+                    if string_replaced[countOccurrences:i].isdigit():
+                        tokens.append("<%s>" % string_replaced[countOccurrences:i])
+                        tokens.append("<%s>" % lexeme)
+                        i += 1
+                        countOccurrences = i
+                    else:
+                        tokens.append("<id, %s>" % array[0])
+                        tokens.append("<%s>" % lexeme)
+                        i += 1
+                        count += 1
+                        countOccurrences = i
+                else:
+                    if string_replaced[countOccurrences:i].isdigit():
+                        tokens.append("<%s>" % string_replaced[countOccurrences:i])
+                        tokens.append("<%s>" % lexeme)
+                        i += 1
+                        countOccurrences = i
+                    else:
+                        table.append(['{0}'.format(count), '{0}'.format(string_replaced[countOccurrences:i])])
+                        tokens.append("<id, %s>" % count)
+                        tokens.append("<%s>" % lexeme)
+                        i += 1
+                        count += 1
+                        countOccurrences = i
 
     elif (i + 1) == string_size:
-        if string_replaced[i - 1].isdigit():
-            tokens.append("<%s>" % string_replaced[countOccurrences:i + 1])
-            i += 1
-            countOccurrences = i
-        else:
-            if string_replaced[countOccurrences:i + 1] in table:
-                print('esta na lista')
-            # table.append('{0}   {1}'.format(count, string_replaced[countOccurrences:i + 1]))
-            table.append(['{0}'.format(count), '{0}'.format(string_replaced[countOccurrences:i + 1])])
-            tokens.append("<id, %s>" % count)
-            i += 1
-            count += 1
-            countOccurrences = i
+        for array in table:
+            if string_replaced[countOccurrences:i + 1] in array:
+                print('string antes: ', string_replaced[countOccurrences:i + 1])
+                if string_replaced[countOccurrences:i + 1].isdigit():
+                    tokens.append("<%s>" % string_replaced[countOccurrences:i + 1])
+                    i += 1
+                    countOccurrences = i
+                else:
+                    tokens.append("<id, %s>" % array[0])
+                    i += 1
+                    count += 1
+                    countOccurrences = i
+            else:
+                print('string: ', string_replaced[countOccurrences:i + 1])
+                # if count == 999:
+                #     tokens.append("<%s>" % string_replaced[countOccurrences:i + 1])
+                #     i += 1
+                #     countOccurrences = i
+                # else:
+                #     # table.append('{0}   {1}'.format(count, string_replaced[countOccurrences:i + 1]))
+                #     table.append(['{0}'.format(count), '{0}'.format(string_replaced[countOccurrences:i + 1])])
+                #     tokens.append("<id, %s>" % count)
+                #     i += 1
+                #     count += 1
+                #     countOccurrences = i
 
     else:
         i += 1
